35fd4ff2e7e05920eacdc19d5b5e9f406268ef72
Prince Bharti <bhartiprince.9971@gmail.com>
2019-01-12 10:29:02 +0530

    Initial Commit

---
+++ tableViewLayoutPractise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewLayoutPractise
+//
+//  Created by Prince Bharti on 12/01/19.
+//  Copyright © 2019 Prince Bharti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
