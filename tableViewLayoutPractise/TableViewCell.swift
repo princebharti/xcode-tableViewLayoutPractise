63999d6df1609d69d09d7072f046d3d00f3d69c5
Prince Bharti <bhartiprince.9971@gmail.com>
2019-01-12 12:41:42 +0530

    completed working

---
+++ tableViewLayoutPractise/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  tableViewLayoutPractise
+//
+//  Created by Prince Bharti on 12/01/19.
+//  Copyright © 2019 Prince Bharti. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+}
--- tableViewLayoutPractise/ViewController.swift
+++ tableViewLayoutPractise/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupTablewView()
+
     }
 
+    @IBAction func showErrorTapped(_ sender: Any) {
+      
+        let sectionFooter = tableView.footerView(forSection: 0) as! FooterView
+        tableView.beginUpdates()
+        sectionFooter.Show()
+        tableView.endUpdates()
+   
+     }
+    
 
+    
+    private func setupTablewView() {
+        tableView.register(UINib(nibName: "FooterView", bundle: nil), forHeaderFooterViewReuseIdentifier: "FooterView")
+        
+    }
+    
+    
+    @IBAction func hideErrorTapped(_ sender: Any) {
+        let sectionFooter = tableView.footerView(forSection: 0) as! FooterView
+        tableView.beginUpdates()
+        sectionFooter.hide()
+        tableView.endUpdates()
+        
+        
+    }
+    
+}
+
+
+
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.label.text = "\(indexPath)"
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let sectionFooter = tableView.dequeueReusableHeaderFooterView(withIdentifier: "FooterView")
+        return sectionFooter
+    }
+   
+    
 }
 
